a4fbaffbd7b6ef9fe10f1cad378b4ff963d3a4f5
adelhassan1 <tomasander12@gmail.com>
2025-07-21 23:12:54 +0300

    allowing vercel monitor preview bot

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,21 +1,31 @@
 import aj from '../config/arcjet.js';
 
+const isVercelPreview = process.env.VERCEL === '1' && process.env.VERCEL_ENV === 'preview';
+
 const arcjetMiddleware = async (req, res, next) => {
+	if (isVercelPreview) {
+		console.warn("🛡️ Arcjet is disabled in Vercel Preview");
+		return next();
+	}
+
 	try {
-    const decision = await aj.protect(req, { requested: 1 });
+		const decision = await aj.protect(req, { requested: 1 });
 
-    if(decision.isDenied()) {
-		if(decision.reason.isRateLimit()) return res.status(429).json({ error: 'Rate limit exceeded' });
-		if(decision.reason.isBot()) return res.status(403).json({ error: 'Bot detected' });
+		if (decision.isDenied()) {
+			if (decision.reason.isRateLimit()) {
+				return res.status(429).json({ error: 'Rate limit exceeded' });
+			}
+			if (decision.reason.isBot()) {
+				return res.status(403).json({ error: 'Bot detected' });
+			}
+			return res.status(403).json({ error: 'Access denied' });
+		}
 
-		return res.status(403).json({ error: 'Access denied' });
-    }
-
-    next();
+		next();
 	} catch (error) {
 		console.log(`Arcjet Middleware Error: ${error}`);
 		next(error);
-}
-}
+	}
+};
 
 export default arcjetMiddleware;
