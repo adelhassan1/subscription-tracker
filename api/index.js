605a2585c976685130572475b8508fb4b00a2a95
adelhassan1 <tomasander12@gmail.com>
2025-07-22 04:18:53 +0300

    test

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(arcjetMiddleware);
+// app.use(arcjetMiddleware);
 
 app.get('/api/health', (req, res) => {
 	console.log("✅ Health check hit");
@@ -23,12 +23,12 @@ app.get('/api/health', (req, res) => {
 });
 
 
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
-app.use('/api/v1/workflows', workflowRouter);
+// app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/auth', authRouter);
+// app.use('/api/v1/subscriptions', subscriptionRouter);
+// app.use('/api/v1/workflows', workflowRouter);
 
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
 	res.send("Welcome to the Subscription Tracker API!");
