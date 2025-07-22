a1b465804b8a2b74bd9c7b52d2eb83d1af4bafa8
adelhassan1 <tomasander12@gmail.com>
2025-07-23 00:37:15 +0300

    test

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 // app.use(arcjetMiddleware);
 
 // app.use('/api/v1/auth', authRouter);
-// app.use('/api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);
 // app.use('/api/v1/subscriptions', subscriptionRouter);
 // app.use('/api/v1/workflows', workflowRouter);
 
